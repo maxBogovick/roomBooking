33bb00753254fff084155bc138345ea08952af5e
Olena Savinkova <owlindacity@gmail.com>
2019-05-02 11:56:31 +0300

    update RegistrationCommand

--- src/main/java/controller/commands/mainCommand/RegistrationCommand.java
+++ src/main/java/controller/commands/mainCommand/RegistrationCommand.java
@@ -1,18 +1,10 @@
 package controller.commands.mainCommand;
 
 import controller.commands.Command;
-import model.dao.DaoFactory;
-import model.dao.impl.JDBCUserDao;
-import model.entity.User;
-import model.entity.types.Role;
 import model.service.ServiceFactory;
 import model.service.UserService;
-import model.service.impl.UserServiceImpl;
-import model.util.Constants;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.SQLException;
 
 public class RegistrationCommand implements Command {
 
@@ -21,13 +13,6 @@ public class RegistrationCommand implements Command {
         String nameFromRequest = request.getParameter("name");
         String passFromRequest = request.getParameter("pass");
         String emailFromRequest = request.getParameter("email");
-
-//        User user = new User();
-//        user.setLogin(request.getParameter("name"));
-//        String passWithHash = Constants.getPwdHash(request.getParameter("pass"));
-//        user.setPassword(passWithHash);
-//        user.setEmail(request.getParameter("email"));
-//        user.setRole(2);
 
 
         UserService userService = ServiceFactory.getUserService();
